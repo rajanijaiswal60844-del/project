38dddca2516cb335de9c5f1c7048f5fa1b1a73c7
Krishna Jaiswal <krishnajaiswal8858825412@gmail.com>
2025-10-20 06:51:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8194887002-c6af6",
+  "appId": "1:682594030521:web:469aab2214480db127dc68",
+  "apiKey": "AIzaSyBZ-1SN9P29acD0VrzIb-lWEfcJSPy3DM4",
+  "authDomain": "studio-8194887002-c6af6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "682594030521"
+};
